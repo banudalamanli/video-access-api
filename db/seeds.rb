a81389cb5c4f720c9a094a2ea8283fcd8e52dd3b
Banu Dalamanli <banudalamanli@gmail.com>
2015-10-13 11:21:42 -0700

    added record to seed and ran db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+actors = ["Tom Cruise","John Smith","Angelina Jolie"]
+directors = ["Steven Spielberg"]
+
+new_vid = Video.create({title: "new title", desc: "new description"})
+new_vid.add_actors(actors)
+new_vid.add_directors(directors)
+
