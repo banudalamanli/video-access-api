d95d1b60b38c41b117aa0e5fcf3d257414c1232e
Banu Dalamanli <banudalamanli@gmail.com>
2015-10-13 11:21:13 -0700

    added methods for creating associated records

--- app/models/video.rb
+++ app/models/video.rb
@@ -3,5 +3,35 @@ class Video < ActiveRecord::Base
 	has_many :person_with_roles, through: :cast_and_crew_members
 	has_many :people, through: :person_with_roles
 
+
 	validates :title, :desc, presence: true
+
+	def actors
+		self.people.actors
+	end
+
+	def directors
+		self.people.directors
+	end
+
+	def add_actors(actors)
+		actors.each do |actor|
+			self.add_cast_or_crew("actor", actor)
+			# self.person_with_roles << Person.create_with_role('actor', name: actor)
+		end
+		self
+	end
+
+	def add_directors(directors)
+		directors.each do |director|
+			self.add_cast_or_crew("director", director)
+			# self.person_with_roles << Person.create_with_role('director', name: director)
+		end
+		self
+	end
+
+	def add_cast_or_crew(role, person_name)
+		self.person_with_roles << Person.create_with_role(role, name: person_name)
+	end
+	
 end
