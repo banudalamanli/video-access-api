39f96558bf5c05d11174f2fbbe54cac80eb60bf7
Banu Dalamanli <banudalamanli@gmail.com>
2015-10-13 08:40:56 -0700

    added create_with_role method

--- app/models/person.rb
+++ app/models/person.rb
@@ -9,4 +9,8 @@ class Person < ActiveRecord::Base
 	scope :role, 			->(role) { inludes(:roles).where(roles: {role: role}).map(&:name) }
 	scope :actors, 		-> { role('actor') }
 	scope :directors, -> { role('director') }
+
+	def self.create_with_role(role, *person)
+		Role.find_or_create_by(role: role).people.create(person)
+	end
 end
