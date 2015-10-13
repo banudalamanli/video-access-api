7d75aab4875cdb30b89f1f8065ff18b0bba2d53a
Banu Dalamanli <banudalamanli@gmail.com>
2015-10-12 22:23:20 -0700

    added associations with JT PersonWithRoles

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,3 +1,6 @@
 class Person < ActiveRecord::Base
+	has_many :person_with_roles, dependent: :destroy
+	has_many :roles, through: :person_with_roles
+	
 	validates :name, presence: true
 end
--- app/models/role.rb
+++ app/models/role.rb
@@ -1,3 +1,8 @@
 class Role < ActiveRecord::Base
+	has_many :person_with_roles, dependent: :destroy
+	has_many :people, through: :person_with_roles
+
 	validates :role, presence:true, uniqueness: true
+	
+	# Should define a method that transforms input to be downcase only to ensure there are no duplicate roles i.e. "Actor" and "actor"
 end
