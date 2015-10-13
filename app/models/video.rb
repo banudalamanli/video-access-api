95947fe032fed8e040a9a6b596a11c1bd1f081c1
Banu Dalamanli <banudalamanli@gmail.com>
2015-10-12 22:30:23 -0700

    added associations

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,3 +1,7 @@
 class Video < ActiveRecord::Base
+	has_many :cast_and_crew_members, dependent: :destroy
+	has_many :person_with_roles, through: :cast_and_crew_members
+	has_many :people, through: :person_with_roles
+
 	validates :title, :desc, presence: true
 end
