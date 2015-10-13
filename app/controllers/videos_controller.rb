dad0735cf569cd572e9334a29bd06f8a8b34a8e6
Banu Dalamanli <banudalamanli@gmail.com>
2015-10-13 16:52:27 -0700

    added error handling for invalid id for #update and #destroy

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,5 +1,6 @@
 class VideosController < ApplicationController
   before_action :set_video, only: [:show, :update, :destroy]
+  rescue_from ActiveRecord::RecordNotFound, with: :invalid_video
 
   # curl -X GET http://localhost:3000/videos
   def index
@@ -14,13 +15,15 @@ class VideosController < ApplicationController
   # curl -X POST --data "title=title&desc=description&actors=Tom+Cruise%2CJohn+Smith%2CAngelina+Jolie&directors=Steven+Spielberg" http://localhost:3000/videos
   def create
     @video = Video.new(title: params[:title], desc: params[:desc])
+
     actors = params[:actors].split(',')
     @video.add_actors(actors)
+
     directors = params[:directors].split(',')
     @video.add_directors(directors)
 
     if @video.save
-      render json: {video: @video.as_json(only: [:title, :desc], methods: [:actors, :directors]), success: true}, status: :created, location: @video
+      render json: { video: @video.as_json(only: [:title, :desc], methods: [:actors, :directors]), success: true }, status: :created, location: @video
     else
       render json: @video.errors, status: :unprocessable_entity
     end
@@ -39,17 +42,13 @@ class VideosController < ApplicationController
 
   # curl -X DELETE http://localhost:3000/videos/1
   def destroy
-    # @video.destroy
+    video_to_delete = @video
+    @video.destroy
 
-    # head :no_content
+    head :no_content
 
     if @video.destroy
-
-      head :no_content
-      render json: @video
-    else
-      @video.error "Invalid video ID"
-      render json: @video
+      render json: { video: video_to_delete.as_json(only: [:title, :desc], methods: [:actors, :directors]), success: true }
     end
   end
 
@@ -61,5 +60,9 @@ class VideosController < ApplicationController
 
     def video_params
       params.require(:video).permit(:title, :desc)
+    end
+
+    def invalid_video
+      render json: { success: false, error: "invalid video ID"}
     end
 end
