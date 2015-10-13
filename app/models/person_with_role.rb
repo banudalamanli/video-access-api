858e15aef19d1a0db66af790f97965cb027745e7
Banu Dalamanli <banudalamanli@gmail.com>
2015-10-12 22:36:03 -0700

    added associations to Video and CastAndCrewMember

--- app/models/person_with_role.rb
+++ app/models/person_with_role.rb
@@ -1,4 +1,6 @@
 class PersonWithRole < ActiveRecord::Base
   belongs_to :person
   belongs_to :role
+  has_many :cast_and_crew_members, dependent: :destroy
+  has_many :videos, through: :cast_and_crew_members
 end
