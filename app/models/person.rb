e2becc39b8101bd0340f8d951f62e27e82e4b527
Banu Dalamanli <banudalamanli@gmail.com>
2015-10-12 22:51:23 -0700

    added role (actor/director) scope(s)

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,6 +1,12 @@
 class Person < ActiveRecord::Base
 	has_many :person_with_roles, dependent: :destroy
 	has_many :roles, through: :person_with_roles
-	
+
 	validates :name, presence: true
+
+	# While trying to find out a way to access associated actors of a video, found out here: http://www.informit.com/articles/article.aspx?p=2220311 
+	# that in addition to being available at the class context, scopes are available automatically on has_many association attributes. Hence:
+	scope :role, 			->(role) { inludes(:roles).where(roles: {role: role}).map(&:name) }
+	scope :actors, 		-> { role('actor') }
+	scope :directors, -> { role('director') }
 end
