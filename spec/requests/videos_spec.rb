00e058a28e6a805d6668030ef46ee99549aaf62a
Banu Dalamanli <banudalamanli@gmail.com>
2015-10-14 12:40:37 -0700

    added test to check if #index returns correct count of videos

--- spec/requests/videos_spec.rb
+++ spec/requests/videos_spec.rb
@@ -6,5 +6,11 @@ RSpec.describe "Videos", type: :request do
       get videos_path
       expect(response).to have_http_status(200)
     end
+
+    it "returns the right count of videos" do
+    	get videos_path
+    	json = JSON.parse(response.body)
+    	expect(json["videos"].length).to eq(Video.all.count)
+    end
   end
 end
