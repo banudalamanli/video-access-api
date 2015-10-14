2a22590d82ad31d2f6615e66e121194326080014
Banu Dalamanli <banudalamanli@gmail.com>
2015-10-14 12:40:54 -0700

    cleaned out comments

--- spec/controllers/videos_controller_spec.rb
+++ spec/controllers/videos_controller_spec.rb
@@ -31,39 +31,11 @@ RSpec.describe VideosController, type: :controller do
     skip("Add a hash of attributes invalid for your model")
   }
 
-  # This should return the minimal set of values that should be in the session
-  # in order to pass any filters (e.g. authentication) defined in
-  # VideosController. Be sure to keep this updated too.
-  let(:valid_session) { {} }
-
   describe "GET #index" do
     it "assigns all videos as @videos" do
       video = Video.create! valid_attributes
       get :index, {}, valid_session
       expect(assigns(:videos)).to eq([video])
-    end
-  end
-
-  describe "GET #show" do
-    it "assigns the requested video as @video" do
-      video = Video.create! valid_attributes
-      get :show, {:id => video.to_param}, valid_session
-      expect(assigns(:video)).to eq(video)
-    end
-  end
-
-  describe "GET #new" do
-    it "assigns a new video as @video" do
-      get :new, {}, valid_session
-      expect(assigns(:video)).to be_a_new(Video)
-    end
-  end
-
-  describe "GET #edit" do
-    it "assigns the requested video as @video" do
-      video = Video.create! valid_attributes
-      get :edit, {:id => video.to_param}, valid_session
-      expect(assigns(:video)).to eq(video)
     end
   end
 
@@ -80,22 +52,12 @@ RSpec.describe VideosController, type: :controller do
         expect(assigns(:video)).to be_a(Video)
         expect(assigns(:video)).to be_persisted
       end
-
-      it "redirects to the created video" do
-        post :create, {:video => valid_attributes}, valid_session
-        expect(response).to redirect_to(Video.last)
-      end
     end
 
     context "with invalid params" do
       it "assigns a newly created but unsaved video as @video" do
         post :create, {:video => invalid_attributes}, valid_session
         expect(assigns(:video)).to be_a_new(Video)
-      end
-
-      it "re-renders the 'new' template" do
-        post :create, {:video => invalid_attributes}, valid_session
-        expect(response).to render_template("new")
       end
     end
   end
@@ -118,12 +80,6 @@ RSpec.describe VideosController, type: :controller do
         put :update, {:id => video.to_param, :video => valid_attributes}, valid_session
         expect(assigns(:video)).to eq(video)
       end
-
-      it "redirects to the video" do
-        video = Video.create! valid_attributes
-        put :update, {:id => video.to_param, :video => valid_attributes}, valid_session
-        expect(response).to redirect_to(video)
-      end
     end
 
     context "with invalid params" do
@@ -131,12 +87,6 @@ RSpec.describe VideosController, type: :controller do
         video = Video.create! valid_attributes
         put :update, {:id => video.to_param, :video => invalid_attributes}, valid_session
         expect(assigns(:video)).to eq(video)
-      end
-
-      it "re-renders the 'edit' template" do
-        video = Video.create! valid_attributes
-        put :update, {:id => video.to_param, :video => invalid_attributes}, valid_session
-        expect(response).to render_template("edit")
       end
     end
   end
@@ -147,12 +97,6 @@ RSpec.describe VideosController, type: :controller do
       expect {
         delete :destroy, {:id => video.to_param}, valid_session
       }.to change(Video, :count).by(-1)
-    end
-
-    it "redirects to the videos list" do
-      video = Video.create! valid_attributes
-      delete :destroy, {:id => video.to_param}, valid_session
-      expect(response).to redirect_to(videos_url)
     end
   end
 
--- spec/routing/videos_routing_spec.rb
+++ spec/routing/videos_routing_spec.rb
@@ -7,28 +7,12 @@ RSpec.describe VideosController, type: :routing do
       expect(:get => "/videos").to route_to("videos#index")
     end
 
-    it "routes to #new" do
-      expect(:get => "/videos/new").to route_to("videos#new")
-    end
-
-    it "routes to #show" do
-      expect(:get => "/videos/1").to route_to("videos#show", :id => "1")
-    end
-
-    it "routes to #edit" do
-      expect(:get => "/videos/1/edit").to route_to("videos#edit", :id => "1")
-    end
-
     it "routes to #create" do
       expect(:post => "/videos").to route_to("videos#create")
     end
 
     it "routes to #update via PUT" do
       expect(:put => "/videos/1").to route_to("videos#update", :id => "1")
-    end
-
-    it "routes to #update via PATCH" do
-      expect(:patch => "/videos/1").to route_to("videos#update", :id => "1")
     end
 
     it "routes to #destroy" do
