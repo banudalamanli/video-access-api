09dd62caa85ff99ea1b8023b0a606722dc992eab
BanuDalamanli <banud@users.noreply.github.com>
2015-10-14 11:22:41 -0700

    added error handling messages

--- app/models/video.rb
+++ app/models/video.rb
@@ -3,22 +3,20 @@ class Video < ActiveRecord::Base
 	has_many :person_with_roles, through: :cast_and_crew_members
 	has_many :people, through: :person_with_roles
 
-
 	validates_presence_of :title, message: "Missing title field"
 	validates_presence_of :desc, message: "Missing description field"
 	
+	def add_cast_or_crew(role, person_name)
+		self.person_with_roles << Person.create_with_role(role, name: person_name)
+	end
 
 	def add_actors(actors)
-		actors.each do |actor|
-			self.add_cast_or_crew("actor", actor)
-		end
+		actors.each { |actor| self.add_cast_or_crew("actor", actor) }
 		self
 	end
 
 	def add_directors(directors)
-		directors.each do |director|
-			self.add_cast_or_crew("director", director)
-		end
+		directors.each { |director| self.add_cast_or_crew("director", director) }
 		self
 	end
 
@@ -30,14 +28,9 @@ class Video < ActiveRecord::Base
 		self.people.directors
 	end
 
-	def add_cast_or_crew(role, person_name)
-		self.person_with_roles << Person.create_with_role(role, name: person_name)
-	end
-
 	def jsonify
 		self.as_json(only: [:title, :desc], methods: [:actors, :directors])
 	end
-	
 end
 
 
