4126b0f98b66d25925130a2710dcebaacbd739ba
Banu Dalamanli <banudalamanli@gmail.com>
2015-10-15 11:16:25 -0700

    added validation tests

--- spec/models/role_spec.rb
+++ spec/models/role_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Role, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to validate_presence_of :role }
+  it { is_expected.to validate_uniqueness_of :role  }
 end
