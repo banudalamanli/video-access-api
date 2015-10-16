1754c6748b28fd5fb3a40112a10d59cb6ceacad7
Banu Dalamanli <banudalamanli@gmail.com>
2015-10-16 15:00:08 -0700

    added validation tests

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -5,5 +5,13 @@ RSpec.describe Video, type: :model do
   it { is_expected.to have_many :person_with_roles }
   it { is_expected.to have_many :people }
 
-  
+  it { is_expected.to validate_presence_of(:title).with_message("Missing title field") }
+  it { is_expected.to validate_presence_of(:desc).with_message("Missing description field") }
 end
+
+
+=begin
+
+http://blog.davidchelimsky.net/blog/2012/02/12/validations-are-behavior-associations-are-structure/
+
+=end
