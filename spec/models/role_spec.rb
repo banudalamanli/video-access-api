fb80e39fb3422714009ac52709502077e8d36fa0
Banu Dalamanli <banudalamanli@gmail.com>
2015-10-19 16:43:33 -0700

    reformatted with context and added detailed validation tests

--- spec/models/role_spec.rb
+++ spec/models/role_spec.rb
@@ -1,9 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe Role, type: :model do
-	it { is_expected.to have_many(:person_with_roles).dependent(:destroy) }
-	it { is_expected.to have_many :people }
+	context "associations" do
+		it { is_expected.to have_many(:person_with_roles).dependent(:destroy) }
+		it { is_expected.to have_many :people }
+	end
 
-  it { is_expected.to validate_presence_of :role }
-  it { is_expected.to validate_uniqueness_of :role  }
+	context 'validations' do
+	  it { is_expected.to validate_presence_of :role }
+	  it { is_expected.to validate_uniqueness_of :role  }
+
+	  describe "presence" do
+	    let(:invalid_role) { Role.create role: "" }
+
+	    it "doesn't save without defining role" do
+	    	expect(invalid_role).to be_invalid
+	    end
+	  end
+
+	  describe "uniqueness" do
+	    let(:duplicate_role) { Role.create role: "writer" }
+	    before { Role.create role: "writer" }
+
+	    it "doesn't save dupes" do
+	    	expect(duplicate_role).to be_invalid
+	    end
+	  end
+		
+	end
 end
