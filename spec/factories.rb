84c185598494f7a93d75aedc89ed6e1e325d390f
Banu Dalamanli <banudalamanli@gmail.com>
2015-10-20 17:50:42 -0700

    added factories :person and :role

---
+++ spec/factories.rb
@@ -0,0 +1,30 @@
+# require ''
+
+FactoryGirl.define do
+  factory :person  do
+    transient do
+      invalid false
+    end
+
+    name { invalid ? "" : Faker::Name.name }
+  end
+
+  factory :role do
+    transient { invalid false }
+
+    roles = ['executive producer', 'screenwriter', 'make-up artist']
+
+    role { invalid ? "" : roles.sample }
+    
+    trait :actor 
+      role "actor"
+    end
+
+    trait :director do
+      role "director"
+    end
+
+    factory :actor, traits: [:actor]
+    factory :director, traits: [:director]
+  end
+end
